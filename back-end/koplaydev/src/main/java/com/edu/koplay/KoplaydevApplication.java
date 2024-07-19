d8fe0c3bb6762986c0837ebf6e3ed6a812f07e03
Jiwan Seo <91694379+wannaseoji@users.noreply.github.com>
2024-07-19 14:59:32 +0900

    git ignore and first settings

---
+++ back-end/koplaydev/src/main/java/com/edu/koplay/KoplaydevApplication.java
@@ -0,0 +1,13 @@
+package com.edu.koplay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KoplaydevApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KoplaydevApplication.class, args);
+    }
+
+}
---
+++ back-end/koplaydev/src/test/java/com/edu/koplay/KoplaydevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.koplay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KoplaydevApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
