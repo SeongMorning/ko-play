714514094e70aec743af6865e6e514250992b992
jeonsm22 <jeonsm22@daum.net>
2024-07-26 10:13:36 +0900

    feat : add animation in promotion page
    프로모션 페이지에 있는 시작버튼에 간단한 애니메이션 넣었습니다.

--- front-end/koplaydev/app/StartButton.js
+++ front-end/koplaydev/app/StartButton.js
@@ -1,6 +1,7 @@
 "use client";
 import styles from "./StartButton.module.scss";
 import { useRouter } from "next/navigation";
+import { motion } from "framer-motion";
 
 export default function StartButton() {
   const router = useRouter();
@@ -9,12 +10,19 @@ export default function StartButton() {
     router.push("/login"); // 로그인 페이지로 라우팅
   };
   return (
-    <div className={styles.startButton} onClick={handleClick}>
-      <div className={styles.startButtonDot} />
-      <div className={styles.startButtonDot2} />
+    <div className={styles.startBtnMain}>
+      <motion.div 
+      className={styles.startButton} 
+      onClick={handleClick}
+      whileHover={{
+        translateY : '1vh'
+      }}>
+        <div className={styles.startButtonDot} />
+        <div className={styles.startButtonDot2} />
+        <div className={styles.startButtonMain} />
+        <h2 className={styles.text}>회원으로 시작</h2>
+      </motion.div>
       <div className={styles.startButtonShadow}></div>
-      <div className={styles.startButtonMain} />
-      <h2 className={styles.text}>회원으로 시작</h2>
     </div>
   );
 }
