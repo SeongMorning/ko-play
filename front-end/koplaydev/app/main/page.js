09a84f994cc87fa8b6463ee0d798dfcf7de4a9a7
jeonsm22 <jeonsm22@daum.net>
2024-07-30 00:32:44 +0900

    feat : main page already finish
    메인페이지 노란 버튼들 제외하고 구성 완료했습니다. 노란 버튼들 추가 후 나머지 모달 작업 하겠습니다.

--- front-end/koplaydev/app/main/page.js
+++ front-end/koplaydev/app/main/page.js
@@ -1,10 +1,14 @@
 import MainBg from "../component/background/MainBg";
+import FameBtn from "./FameBtn";
 import MainIcons from "./MainIcons";
+import MainMenu from "./MainMenu";
 import Profile from "./Profile";
 
 export default function Test() {
   return (
     <>
+      <FameBtn/>
+      {/* <MainMenu/> */}
       <MainBg />
       <MainIcons/>
       <Profile/>
