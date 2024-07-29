d877d2e9b4b566b8e5b02f2941f8bba31203e020
jeonsm22 <jeonsm22@daum.net>
2024-07-29 15:48:29 +0900

    feat : add main page
    메인페이지 관련 컴포넌트 제작 및 메인페이지 구성 진행중입니다.

--- front-end/koplaydev/app/test/page.js
+++ front-end/koplaydev/app/test/page.js
@@ -1,38 +1,13 @@
 import MainBg from "../component/background/MainBg";
-import styles from './page.module.scss';
+import MainIcons from "./MainIcons";
+import Profile from "./Profile";
 
 export default function Test() {
   return (
     <>
       <MainBg />
-      <svg
-      className={styles.expBar}
-        xmlns="http://www.w3.org/2000/svg"
-        width="1728"
-        height="167"
-        viewBox="0 0 1728 167"
-        fill="none"
-      >
-        <path
-          d="M465.85 1L-1 90.474V140L465.85 36.1711H1291.24L1728 166V103.474L1291.24 1H465.85Z"
-          fill="url(#paint0_linear_573_373)"
-          stroke="white"
-        />
-        <defs>
-          <linearGradient
-            id="paint0_linear_573_373"
-            x1="-1"
-            y1="83.5"
-            x2="1728"
-            y2="83.5"
-            gradientUnits="userSpaceOnUse"
-          >
-            <stop stopColor="#FF8B8B" />
-            <stop offset="0.306" stopColor="#969AFF" stopOpacity="0.5" />
-            <stop offset="1" stopColor="white" />
-          </linearGradient>
-        </defs>
-      </svg>
+      <MainIcons/>
+      <Profile/>
     </>
   );
 }
