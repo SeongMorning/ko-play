050351c77cf21a5eb3a2fefc57f09ab1d8a78bda
seokjr <116258012+seokjr@users.noreply.github.com>
2024-07-31 12:25:33 +0900

    style : AlbumFameBg rename
    앨범에서만 쓰이는 것 같아서 이름 수정

--- front-end/koplaydev/app/component/background/AlbumFameBg.js
+++ front-end/koplaydev/app/component/background/AlbumFameBg.js
@@ -1,14 +1,14 @@
 import styles from "./AlbumFameBg.module.scss";
-import Star from "../Star";
+import StarBg from "../StarBg";
 export default function AlbumFameBg() {
   return (
     <>
-      <Star left="10vw" top="20vh" duration="2" />
-      <Star left="5vw" top="50vh" duration="2" />
-      <Star left="20vw" top="80vh" duration="1.5" />
-      <Star right="10vw" top="20vh" duration="1.5" />
-      <Star right="50vw" top="5vh" duration="1.5" />
-      <Star right="5vw" top="80vh" duration="2" />
+      <StarBg left="10vw" top="20vh" duration="2" />
+      <StarBg left="5vw" top="50vh" duration="2" />
+      <StarBg left="20vw" top="80vh" duration="1.5" />
+      <StarBg right="10vw" top="20vh" duration="1.5" />
+      <StarBg right="50vw" top="5vh" duration="1.5" />
+      <StarBg right="5vw" top="80vh" duration="2" />
       <img className={styles.constellation1} src="Constellation1.png" />
       <img className={styles.constellation2} src="Constellation2.png" />
       <img className={styles.constellation3} src="Constellation3.png" />
