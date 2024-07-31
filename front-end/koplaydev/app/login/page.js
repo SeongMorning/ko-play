7be870e5ebfd1c3ebae9c8113a869e06871f1883
seokjr <116258012+seokjr@users.noreply.github.com>
2024-07-31 21:34:53 +0900

    fix : login page edit
    LoginBox -> StudentLogin
    ParentLogin 분할 예정
    로그인 버튼 눌렀을 때 SelectStatus가 로고 밑에 생기고 버튼 누른 것대로 학생인지 부모인지 선택하게 됨.
    login 페이지에 use client 사용중. 상태관리 방법 고안중.

--- front-end/koplaydev/app/login/page.js
+++ front-end/koplaydev/app/login/page.js
@@ -1,28 +1,58 @@
+"use client";
+import { useState } from "react";
 import styles from "./page.module.scss";
 import JellyBtn from "./component/JellyBtn";
 import PromLoginBg from "../component/background/PromLoginBg";
+import StudentLogin from "./component/StudentLogin";
+import SelectStatus from "./component/SelectStatus";
 
 export default function Login() {
+  const [isStudentSelected, setIsStudentSelected] = useState(false);
+  const [isParentSelected, setIsParentSelected] = useState(false);
+
+  const handleStudentClick = () => {
+    setIsStudentSelected(true);
+    setIsParentSelected(false);
+  };
+
+  const handleParentClick = () => {
+    setIsParentSelected(true);
+    setIsStudentSelected(false);
+  };
+
   return (
     <>
       <main className={styles.main}>
         <img className={styles.logo} src="/logo.png" />
-        <div className={styles.statusButtonContainer}>
-          <JellyBtn
-            width={""}
-            height={""}
-            bg={"#ffd6e0"}
-            shadow={"#e07a93"}
-            text={"부모님"}
-          />
-          <JellyBtn
-            width={""}
-            height={""}
-            bg={"#A2D2FF"}
-            shadow={"#4DA3F3"}
-            text={"학생"}
-          />
-        </div>
+        {isStudentSelected || isParentSelected ? (
+          <>
+            <SelectStatus
+              text={isStudentSelected ? "학생" : "부모님"}
+              isSelected={true}
+              onClick={() => {}}
+            />
+            <StudentLogin />
+          </>
+        ) : (
+          <div className={styles.statusButtonContainer}>
+            <JellyBtn
+              width={""}
+              height={""}
+              bg={"#ffd6e0"}
+              shadow={"#e07a93"}
+              text={"부모님"}
+              onClick={handleParentClick}
+            />
+            <JellyBtn
+              width={""}
+              height={""}
+              bg={"#A2D2FF"}
+              shadow={"#4DA3F3"}
+              text={"학생"}
+              onClick={handleStudentClick}
+            />
+          </div>
+        )}
       </main>
       <PromLoginBg />
     </>
