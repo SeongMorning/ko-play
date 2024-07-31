ef92ecb82df9f5632ccbffceb64b52a86525c505
seokjr <116258012+seokjr@users.noreply.github.com>
2024-07-31 22:31:45 +0900

    fix : JellyBtn cursor, ParentLogin fix, SelectStatus fix
    젤리 버튼 커서 추가
    parentLogin 컴포넌트 커서 추가
    SelectStatus 컴포넌트 상태 수정

--- front-end/koplaydev/app/login/component/SelectStatus.js
+++ front-end/koplaydev/app/login/component/SelectStatus.js
@@ -1,19 +1,23 @@
 "use client";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import ParentStudentBtn from "./ParentStudentBtn";
 
-export default function SelectStatus(props) {
-  const [selectedButton, setSelectedButton] = useState(props.text);
+export default function SelectStatus({ initialText, onSelect }) {
+  const [selectedButton, setSelectedButton] = useState(initialText);
+
+  useEffect(() => {
+    onSelect(selectedButton);
+  }, [selectedButton, onSelect]);
 
   return (
     <div>
       <ParentStudentBtn
-        text={"부모님"}
+        text="부모님"
         isSelected={selectedButton === "부모님"}
         onClick={() => setSelectedButton("부모님")}
       />
       <ParentStudentBtn
-        text={"학생"}
+        text="학생"
         isSelected={selectedButton === "학생"}
         onClick={() => setSelectedButton("학생")}
       />
