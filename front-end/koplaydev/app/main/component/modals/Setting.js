2e26dfef3141965050f335bcc82906d6f3f0b283
jeonsm22 <jeonsm22@daum.net>
2024-08-01 17:51:26 +0900

    fix : fix many bug
    모든 페이지 곳곳에서 발생하던 버그들 수정했습니다.

--- front-end/koplaydev/app/main/component/modals/Setting.js
+++ front-end/koplaydev/app/main/component/modals/Setting.js
@@ -8,16 +8,18 @@ export default function Setting() {
   const dispatch = useDispatch();
   return (
     <YellowBox width={"30"} height={"80"}>
-      <img 
+      <img
         src="/close.png"
         className={styles.backBtn}
         onClick={() => {
           dispatch(changeModalIdx(0));
         }}
-      >
-      </img>
+      ></img>
       <div className={styles.settingMain}>
-        <img className={styles.profileImg} src="/hehe.png" />
+        <div className={styles.profileBox}>
+          <img className={styles.profileImg} src="/hehe.png" />
+          <img className={styles.profileSetting}src="/settingIcon2.png"/>
+        </div>
         <WhiteBox width={"60"} height={"10"}>
           <input />
           <img className={styles.modifyImg} src="/modify.png" />
@@ -27,7 +29,7 @@ export default function Setting() {
           <img className={styles.modifyImg} src="/modify.png" />
         </WhiteBox>
         <WhiteBox width={"60"} height={"10"}>
-          비밀번호 재설정
+          <span>비밀번호 재설정</span>
         </WhiteBox>
       </div>
     </YellowBox>
