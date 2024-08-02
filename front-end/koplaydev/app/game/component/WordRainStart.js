1a069083ed7f672fcf4ec5fec9cad3c370076697
jeonsm22 <jeonsm22@daum.net>
2024-08-02 19:18:30 +0900

    feat : add WordRain game (50%)
    단어비 게임 작업중입니다.
    진행까지는 완료했고 데이터 뽑는 작업 진행중입니다.

--- front-end/koplaydev/app/game/component/WordRainStart.js
+++ front-end/koplaydev/app/game/component/WordRainStart.js
@@ -1,17 +1,102 @@
-'use client'
+"use client";
 
-import CardFrontImage from './CardFrontImage';
-import styles from './WordRainStart.module.scss';
+import { useCallback, useEffect, useRef, useState } from "react";
+import CardFrontImage from "./CardFrontImage";
+import styles from "./WordRainStart.module.scss";
+import { motion } from "framer-motion";
+import { current } from "@reduxjs/toolkit";
 
-export default function WordRainStart(){
-    const wordObjectList = [
-        
-    ]
+export default function WordRainStart() {
+  const [wordObjectList, setWordObjectList] = useState([
+    {
+      left: Math.random() * 60 + 10,
+      imgSrc: "/korea-3.png",
+    },
+    {
+      left: Math.random() * 60 + 10,
+      imgSrc: "/korea-3.png",
+    },
+    {
+      left: Math.random() * 60 + 10,
+      imgSrc: "/korea-3.png",
+    },
+    {
+      left: Math.random() * 60 + 10,
+      imgSrc: "/korea-3.png",
+    },
+    {
+      left: Math.random() * 60 + 10,
+      imgSrc: "/korea-3.png",
+    },
+    {
+      left: Math.random() * 60 + 10,
+      imgSrc: "/korea-3.png",
+    },
+    {
+      left: Math.random() * 60 + 10,
+      imgSrc: "/korea-3.png",
+    },
+    {
+      left: Math.random() * 60 + 10,
+      imgSrc: "/korea-3.png",
+    },
+    {
+      left: Math.random() * 60 + 10,
+      imgSrc: "/korea-3.png",
+    },
+    {
+      left: Math.random() * 60 + 10,
+      imgSrc: "/korea-3.png",
+    },
+  ]);
 
-    return(
-        <div>
-            <CardFrontImage top="-17" left="0" width="10" height="17" imgSrc="/korea-3.png"/>
+  const [resultList, setResultList] = useState([0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+  const [copy, setCopy] = useState(resultList);
+  const [wrong, setWrong] = useState([]);
+  const [cnt, setCnt] = useState(0);
 
-        </div>
-    )
+  const changeResultList = async (index) => {
+    setCnt(cnt+1);
+    if(resultList[index] !== 1){
+        let copy2 = [...copy]
+        copy2[index] = -1;
+        setCopy(copy2);
+        let wrong2 = [...wrong];
+        wrong2.push(wordObjectList[index]);
+        setWrong(wrong2);
+        setResultList(copy);
+    }
+  };
+
+  useEffect(()=>{
+    ()=>console.log(resultList),1000
+  },[resultList])
+
+  return (
+    <div>
+      {wordObjectList.map((data, index) => {
+        return (
+          <motion.div
+            className={styles.CardMain}
+            style={{
+              left: `${data.left}%`,
+              top: "-17%",
+              width: "10%",
+              height: "17%",
+            }}
+            animate={{
+              translateY: "118vh",
+              transition: {
+                duration: 5,
+                delay: index * 1,
+              },
+            }}
+            onViewportLeave={() => changeResultList(index)}
+          >
+            <CardFrontImage imgSrc={data.imgSrc} />
+          </motion.div>
+        );
+      })}
+    </div>
+  );
 }
