f2218e4121eee1cb509567c72ea5fee82ad6b39c
Jiwan Seo <91694379+wannaseoji@users.noreply.github.com>
2024-08-02 20:04:59 +0900

    feat : profile 받아오기 axios
    feat : profile 받아오기 axios
    
    아직 에러 존재 - alert

--- front-end/koplaydev/app/login/component/StudentLogin.js
+++ front-end/koplaydev/app/login/component/StudentLogin.js
@@ -1,21 +1,22 @@
 "use client";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useRouter } from "next/navigation";
 import styles from "./StudentLogin.module.scss";
 import axios from 'axios';
+import { useDispatch } from "react-redux";
+import { changeTokenIdx } from "@/redux/slices/tokenSlice";
 
 export default function StudentLogin() {
   const router = useRouter();
-  
+  const dispatch = useDispatch();
+
   // 각각의 입력 필드에 대한 useRef 선언
   const idRef = useRef(null);
   const passwordRef = useRef(null);
-  let id_in = '';
-  let pw_in = '';
-  useEffect(()=>{
-    id_in = idRef.current.value;
-    pw_in = passwordRef.current.value;
-  },[idRef,passwordRef])
+
+  // 상태 변수로 token 관리
+  const [token, setToken] = useState(null);
+
   // 쿠키에서 특정 값을 가져오는 함수
   function getCookieValue(name) {
     const cookieArray = document.cookie.split(';');
@@ -28,33 +29,44 @@ export default function StudentLogin() {
     return null;
   }
 
+  // token 변경 시 Redux 상태 업데이트
+  useEffect(() => {
+    if (token) {
+      dispatch(changeTokenIdx(token)); // Redux 상태 업데이트
+    }
+  }, [token, dispatch]); // token이나 dispatch가 변경될 때마다 실행
+
   // 버튼 클릭 시 실행되는 함수
   const handleClick = () => {
     const formData = new FormData();
-    
+
     // 입력 필드의 값을 올바르게 가져와서 FormData에 추가
-    formData.append('id', idRef.current.value);  // 올바른 접근 방법
+    formData.append('id', idRef.current.value);
     formData.append('password', passwordRef.current.value);
 
     // 값 확인을 위해 콘솔에 출력
     console.log("ID:", idRef.current.value, "Password:", passwordRef.current.value);
 
-    axios.post('http://localhost:8080/login', 
-      formData, { withCredentials: true }
+    axios.post('http://localhost:8080/login',
+      formData, { withCredentials: true },
     )
     .then((res) => {
       console.log('Response:', res);
 
       // 쿠키에서 Authorization 토큰 가져오기
-      const token = getCookieValue('Authorization');
-      console.log('Auth Token:', token);
-
+      const authToken = getCookieValue('Authorization');
+      if(authToken == null){
+        throw error;
+      }
+      setToken(authToken); // 상태 업데이트로 token 설정
+      
+      console.log('Auth Token:', authToken);
       // 성공적으로 로그인 시 메인 페이지로 이동
       router.push("/main");
     })
     .catch((e) => {
       alert("로그인 실패");
-      console.error('Error:', e);
+      console.log(e);
     });
   };
 
@@ -64,14 +76,14 @@ export default function StudentLogin() {
         <input
           className={styles.idInput}
           type="text"
-          id="username"  // 고유한 ID 설정
+          id="username" // 고유한 ID 설정
           ref={idRef}
           placeholder="아이디를 입력하세요"
         />
         <input
           className={styles.pwInput}
           type="password"
-          id="password"  // 고유한 ID 설정
+          id="password" // 고유한 ID 설정
           ref={passwordRef}
           placeholder="비밀번호를 입력하세요"
           required
