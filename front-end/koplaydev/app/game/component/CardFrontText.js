285c03361141bbe16ab9cd67a6726e2e8ea5fc2a
seokjr <116258012+seokjr@users.noreply.github.com>
2024-08-02 13:14:10 +0900

    feat : CardFront component add
    카드 앞면 이미지, 텍스트 컴포넌트 추가

---
+++ front-end/koplaydev/app/game/component/CardFrontText.js
@@ -0,0 +1,23 @@
+"use client";
+import styles from "./CardFrontText.module.scss";
+
+// props : left, top, width, height
+export default function CardFrontText(props) {
+  return (
+    <div
+      className={styles.cardContainer}
+      styles={{
+        left: `${props.left}`,
+        top: `${props.top}`,
+        width: `${props.width}`,
+        height: `${props.height}`,
+      }}
+    >
+      <div className={styles.cardOuter}>
+        <div className={styles.cardInner}>
+          <span>1</span>
+        </div>
+      </div>
+    </div>
+  );
+}
