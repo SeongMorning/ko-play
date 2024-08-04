4b3de9e33d39f0ebb488c45f943ed6a82befde62
jiyoung-Heo <hgyoung06@gmail.com>
2024-08-04 16:03:37 +0900

    feat: Add logout axios
    axios 로그아웃 기능 추가

--- front-end/koplaydev/app/login/page.js
+++ front-end/koplaydev/app/login/page.js
@@ -7,32 +7,12 @@ import PromLoginBg from "../component/background/PromLoginBg";
 import StudentLogin from "./component/StudentLogin";
 import SelectStatus from "./component/SelectStatus";
 import ParentLogin from "./component/ParentLogin";
-// import Loading from "../game/loading";
-
-// async function fetchData() {
-//   // 실제 데이터 패칭
-//   await new Promise((resolve) => setTimeout(resolve, 3000)); // 1초 대기 (데이터 패칭 시뮬레이션)
-// }
 
 export default function Login() {
   const [selectedStatus, setSelectedStatus] = useState("");
-  // const [loading, setLoading] = useState(true); // 로딩 상태 추가
-
-  // useEffect(() => {
-  //   const loadData = async () => {
-  //     await fetchData(); // 데이터 패칭
-  //     setLoading(false);  // 로딩 완료
-  //   };
-  //   loadData();
-  // }, []);
-
   const handleSelectStatus = (status) => {
     setSelectedStatus(status);
   };
-
-  // if(loading){
-  //   return <Loading/>
-  // }
 
   return (
     <>
