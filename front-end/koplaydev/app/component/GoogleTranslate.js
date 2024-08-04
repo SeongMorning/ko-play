bc77de183135be24a52a04ec7e3a938469c0a28d
seokjr <116258012+seokjr@users.noreply.github.com>
2024-08-04 15:37:19 +0900

    feat : GoogleTranslate component
    구글 번역기 컴포넌트 추가.

---
+++ front-end/koplaydev/app/component/GoogleTranslate.js
@@ -0,0 +1,37 @@
+"use client";
+import { useEffect } from "react";
+
+export default function GoogleTranslate() {
+  const googleTranslateElementInit = () => {
+    if (!window.google || !window.google.translate) return;
+    new window.google.translate.TranslateElement(
+      {
+        pageLanguage: "en",
+        autoDisplay: false,
+      },
+      "google_translate_element"
+    );
+  };
+
+  useEffect(() => {
+    if (!document.querySelector("#google_translate_script")) {
+      const addScript = document.createElement("script");
+      addScript.id = "google_translate_script";
+      addScript.setAttribute(
+        "src",
+        "//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"
+      );
+      document.body.appendChild(addScript);
+      window.googleTranslateElementInit = googleTranslateElementInit;
+    } else {
+      googleTranslateElementInit();
+    }
+  }, []);
+
+  return (
+    <div
+      id="google_translate_element"
+      style={{ display: "inline-block" }}
+    ></div>
+  );
+}
