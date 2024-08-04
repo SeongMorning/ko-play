f2a5d5eefe5fac271b9e915dfa75af0336c65fb7
jiyoung-Heo <hgyoung06@gmail.com>
2024-08-05 01:55:37 +0900

    feat: add redux-persist for save localstorage
    새로고침 시 데이터 유지를 위해
    redux-persist 라이브러리를 통해 localstorage에 데이터를 저장한다.

--- front-end/koplaydev/redux/store.js
+++ front-end/koplaydev/redux/store.js
@@ -1,36 +1,37 @@
-import { configureStore } from "@reduxjs/toolkit";
-import modal from "./slices/modalSlice";
-import game from "./slices/gameSlice";
-import test from "./slices/testSlice";
-import myPage from "./slices/myPageSlice";
-import loading from "./slices/loadingSlice";
-import token from "./slices/tokenSlice";
-import correct from "./slices/correct";
-import studentInfo from "./slices/studentInfoSlice"
-import wrong from "./slices/wrongList";
-import incorrect from "./slices/Incorrect";
-import avatarSlice from "./slices/avatarSlice";
-import studentAvatarSlice from "./slices/studentAvatarSlice";
-import level from "./slices/levelSlice";
-import graphLevel from "./slices/graphLevel";
+import { combineReducers } from '@reduxjs/toolkit';
+import modal from './slices/modalSlice';
+import game from './slices/gameSlice';
+import test from './slices/testSlice';
+import myPage from './slices/myPageSlice';
+import loading from './slices/loadingSlice';
+import token from './slices/tokenSlice';
+import correct from './slices/correct';
+import studentInfo from './slices/studentInfoSlice';
+import wrong from './slices/wrongList';
+import incorrect from './slices/Incorrect';
+import avatarSlice from './slices/avatarSlice';
+import studentAvatarSlice from './slices/studentAvatarSlice';
+import level from './slices/levelSlice';
+import graphLevel from './slices/graphLevel';
 import exp from "./slices/expSlice";
 
-export const store = configureStore({
-    reducer : {
-        modal : modal.reducer,
-        game : game.reducer,
-        test : test.reducer,
-        myPage : myPage.reducer,
-        level : level.reducer,
-        loading : loading.reducer,
-        token : token.reducer,
-        correct : correct.reducer,
-        studentInfo : studentInfo.reducer,
-        wrong : wrong.reducer,
-        incorrect : incorrect.reducer,
-        avatar : avatarSlice.reducer,
-        myAvatar : studentAvatarSlice.reducer,
-        graphLevel : graphLevel.reducer,
-        exp : exp.reducer,
-    },
+const rootReducer = combineReducers({
+  modal: modal.reducer,
+  game: game.reducer,
+  test: test.reducer,
+  myPage: myPage.reducer,
+  level: level.reducer,
+  loading: loading.reducer,
+  token: token.reducer,
+  correct: correct.reducer,
+  studentInfo: studentInfo.reducer,
+  wrong: wrong.reducer,
+  incorrect: incorrect.reducer,
+  avatar: avatarSlice.reducer,
+  myAvatar: studentAvatarSlice.reducer,
+  graphLevel: graphLevel.reducer,
+  exp : exp.reducer,
+
 });
+
+export default rootReducer;
