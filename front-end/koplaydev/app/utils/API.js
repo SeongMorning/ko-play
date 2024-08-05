e16e31e200912d690a75053e0927b9426d41cf92
jiyoung-Heo <hgyoung06@gmail.com>
2024-08-05 14:40:26 +0900

    fix: test cicd

--- front-end/koplaydev/app/utils/API.js
+++ front-end/koplaydev/app/utils/API.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const API = axios.create({
-	baseURL: 'http://localhost:8080',
+	// baseURL: 'http://localhost:8080',
+    baseURL: 'http://backend:8080',
     withCredentials: true,
 });
 
