1063ddcf6ebe37da99b47cb4c3aa1d124d2433e9
kjiin <156158673+kjiin@users.noreply.github.com>
2024-08-06 23:17:08 +0900

    fix : Fix FlipFlip game exp
    FlipFlip 게임에 경험치 반영 부분 수정

--- front-end/koplaydev/app/game/component/flipflip/FlipFlipGameJellyBtn.js
+++ front-end/koplaydev/app/game/component/flipflip/FlipFlipGameJellyBtn.js
@@ -69,7 +69,7 @@ export default function FlipFlipGameJellyBtn(props) {
           dispatch(changeExp(unitScore * correctCnt))
           dispatch(changeInCorrect(true));
           dispatch(changeLoadingIdx(1));
-        }else if(props.text === "아니요"){
+        }else if(props.text === "아니요" || props.text === "확인"){
           dispatch(changeExp(Math.round((unitScore * correctCnt) / 2)))
           dispatch(changeInCorrect(false));
           dispatch(changeLoadingIdx(1));
