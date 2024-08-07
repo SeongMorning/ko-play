19af3b24538f30e8456bef8173f5b21e2b33c0c9
jeonsm22 <jeonsm22@daum.net>
2024-08-07 17:53:11 +0900

    fix : bug
    bug

--- front-end/koplaydev/app/game/[id]/page.js
+++ front-end/koplaydev/app/game/[id]/page.js
@@ -8,9 +8,7 @@ import RankTest from "../component/rankgame/rankTest";
 //   await new Promise((resolve) => setTimeout(resolve, 5000)); // 5초 대기 (데이터 패칭 시뮬레이션)
 // }
 
-// 단어비 게임 페이지.
 export default async function game({ params }) {
-  await fetchData();
 
   return <GameSelector gameidx={params.id} />;
 }
---
+++ front-end/koplaydev/app/websocket/page.js
@@ -0,0 +1,49 @@
+'use client';
+
+import { useEffect, useRef, useState } from 'react';
+import SockJS from 'sockjs-client';
+import { Stomp } from '@stomp/stompjs';
+
+const RankTest = () => {
+    const [message, setMessage] = useState('');
+    const stompClientRef = useRef(null);
+    const connectedRef = useRef(false); // 연결 상태를 추적하는 ref
+
+    useEffect(() => {
+        const socket = new SockJS('http://localhost:8080/ws-stomp');
+        const stompClient = Stomp.over(socket);
+        
+        stompClientRef.current = stompClient;
+        stompClient.connect({}, (frame) => {
+            console.log('Connected: ' + frame);
+            connectedRef.current = true; // 연결 상태를 true로 설정
+            stompClient.subscribe('/room/greetings', (greeting) => {
+                setMessage(JSON.parse(greeting.body).content);
+            });
+        });
+
+        return () => {
+            if (stompClientRef.current) {
+                stompClientRef.current.disconnect();
+                connectedRef.current = false; // 연결 상태를 false로 설정
+            }
+        };
+    }, []);
+
+    const sendName = () => {
+        if (connectedRef.current && stompClientRef.current) {
+            stompClientRef.current.send('/send/hello', {}, JSON.stringify({ name: 'World' }));
+        } else {
+            console.log('STOMP client is not connected.');
+        }
+    };
+
+    return (
+        <div>
+            {/* <button onClick={sendName}>Send Hello</button>
+            <div>{message}</div> */}
+        </div>
+    );
+};
+
+export default RankTest;
