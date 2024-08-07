6a4785a123ed4bbfd18d0753b7065872568b30b4
jiyoung-Heo <hgyoung06@gmail.com>
2024-08-07 23:04:21 +0900

    Squashed commit of the following:
    commit d61ddc82d7b9e1aaa41300eaa45e88c0b0d47fc2
    Merge: 1bc26b3 031bdb3
    Author: 석재륜 <ahddol@naver.com>
    Date:   Wed Aug 7 22:29:45 2024 +0900
    
        Merge branch '석재륜' into 'master'
    
        fix : cabinet logic edit
    
        See merge request s11-webmobile1-sub2/S11P12B302!274
    
    commit 031bdb38c94113d931bb8fc82710c2e26f62ef7d
    Author: seokjr <116258012+seokjr@users.noreply.github.com>
    Date:   Wed Aug 7 22:29:33 2024 +0900
    
        fix : cabinet logic edit
    
        내가 소유한 것들은 색있게 나오기 성공.
    
    commit 1bc26b3b0a5bf97a056ea5aedeee183d93c10f7f
    Merge: 5e7acbb 07db767
    Author: 전성모 <jeonsm22@daum.net>
    Date:   Wed Aug 7 21:08:58 2024 +0900
    
        Merge branch '전성모' into 'master'
    
        feat : websocket connect
    
        See merge request s11-webmobile1-sub2/S11P12B302!273
    
    commit 07db767d7ebc8ecee65865d1ca2a46d03f26bee2
    Author: jeonsm22 <jeonsm22@daum.net>
    Date:   Wed Aug 7 21:08:27 2024 +0900
    
        feat : websocket connect
    
        상태공유하는 것 까지 확인하고 올립니다.
    
    commit 5e7acbbbf9837ba9329067c232aa563c25dad232
    Merge: 8dbc2da cda5b9e
    Author: 허지영 <hgyoung06@gmail.com>
    Date:   Wed Aug 7 21:00:26 2024 +0900
    
        Merge branch '허지영' into 'master'
    
        fix: change jpa query bug
    
        See merge request s11-webmobile1-sub2/S11P12B302!272
    
    commit 8dbc2dac27c6e6cb691a44b271228aed19f8b18d
    Merge: 24b6238 9a3738c
    Author: 허지영 <hgyoung06@gmail.com>
    Date:   Wed Aug 7 20:59:06 2024 +0900
    
        Merge branch '허지영' into 'master'
    
        fix: change jpa query bug
    
        See merge request s11-webmobile1-sub2/S11P12B302!271

--- front-end/koplaydev/app/websocket/page.js
+++ front-end/koplaydev/app/websocket/page.js
@@ -8,16 +8,19 @@ const RankTest = () => {
     const [message, setMessage] = useState('');
     const stompClientRef = useRef(null);
     const connectedRef = useRef(false); // 연결 상태를 추적하는 ref
-
+    useEffect(()=>{
+        console.log(message);
+        setMessage("12")
+    }, [message])
+    
     useEffect(() => {
-        const socket = new SockJS('http://localhost:8080/ws-stomp');
+        const socket = new SockJS('http://localhost:8080/gs-guide-websocket');
         const stompClient = Stomp.over(socket);
-        
         stompClientRef.current = stompClient;
         stompClient.connect({}, (frame) => {
             console.log('Connected: ' + frame);
             connectedRef.current = true; // 연결 상태를 true로 설정
-            stompClient.subscribe('/room/greetings', (greeting) => {
+            stompClient.subscribe('/topic/greetings', (greeting) => {
                 setMessage(JSON.parse(greeting.body).content);
             });
         });
@@ -32,7 +35,7 @@ const RankTest = () => {
 
     const sendName = () => {
         if (connectedRef.current && stompClientRef.current) {
-            stompClientRef.current.send('/send/hello', {}, JSON.stringify({ name: 'World' }));
+            stompClientRef.current.send('/app/hello', {}, JSON.stringify({ name: 'World' }));
         } else {
             console.log('STOMP client is not connected.');
         }
@@ -40,8 +43,8 @@ const RankTest = () => {
 
     return (
         <div>
-            {/* <button onClick={sendName}>Send Hello</button>
-            <div>{message}</div> */}
+            <button onClick={sendName}>Send Hello</button>
+            <div>{message}</div>
         </div>
     );
 };
