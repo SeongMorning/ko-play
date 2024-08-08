ca4b56722be8457419454d0cf44b6795ca37982f
Jiwan Seo <91694379+wannaseoji@users.noreply.github.com>
2024-08-08 15:38:37 +0900

    feat : gameMessage
    feat : gameMessage
    
    Word, left, state 반환하도록 수정

--- back-end/koplaydev/src/main/java/com/edu/koplay/websocket/GameWordMessage.java
+++ back-end/koplaydev/src/main/java/com/edu/koplay/websocket/GameWordMessage.java
@@ -4,14 +4,12 @@ import com.edu.koplay.dto.ResponseDTO;
 import com.edu.koplay.dto.WordDTO;
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.messaging.Message;
-import org.springframework.messaging.core.MessagePostProcessor;
 
 @Setter
 @Getter
 public class GameWordMessage {
-    ResponseDTO<WordDTO> message;
-    public GameWordMessage(ResponseDTO<WordDTO> response) {
+    ResponseDTO<Object> message;
+    public GameWordMessage(ResponseDTO<Object> response) {
         this.message = response;
 
     }
