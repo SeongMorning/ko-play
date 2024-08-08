ac391d319ef1ac3aa5d3e6cba90eadb9ddad9669
jiyoung-Heo <hgyoung06@gmail.com>
2024-08-08 21:59:44 +0900

    feat: Add axios parent statistic page
    부모페이지 통계부분 코드정리하고 axios 넣었습니다
    
    css는 못하겠어요,,,,,,

--- front-end/koplaydev/app/parent/component/DetailpageChildProfileCard.js
+++ front-end/koplaydev/app/parent/component/DetailpageChildProfileCard.js
@@ -1,13 +1,23 @@
 "use client";
 import { useRouter } from "next/navigation";
-import { useState } from "react";
-import DetailBox from "./DetailBox";
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 import styles from "./DetailpageChildProfileCard.module.scss";
 import CheckChildInfo from "./CheckChildInfo";
+import Correct from "../child/[id]/statistic/component/Correct";
+import parentChildStatisticsAxios from "@/app/axios/parentChildStatisticsAxios";
+import ClickedPinkBtn from "../child/[id]/component/ClickedPinkBtn";
+import YellowBox from "../child/[id]/component/YellowBox";
+import { changeParentChaildStatistic } from "@/redux/slices/parentChaildStatisticSlice";
 
 export default function DetailpageChildProfileCard({ child }) {
+    const parent = useSelector((state) => state.parent)
     const router = useRouter();
     const [openModal, setOpenModal] = useState(false);
+    const [selectedIndex, setSelectedIndex] = useState(null);
+
+    let myPageList = ["정답률", "진도 현황", "성취도 비교", "스냅샷"];
+
 
     const clickIcon = () => {
         setOpenModal(true);
@@ -17,9 +27,26 @@ export default function DetailpageChildProfileCard({ child }) {
         setOpenModal(false);
     };
 
-    const handleDetailBoxClick = (view) => {
-        router.push(`/parent/child/${child.id}/statistic?view=${view}`);
-    };
+    useEffect(() => {
+        const fetchstudentStatistics = async () => {
+            const res = await parentChildStatisticsAxios(child.id);
+            changeParentChaildStatistic(res);
+        };
+        fetchstudentStatistics();
+    }, []);
+
+
+    const MyPageSelector = (props) => {
+        const [statistic, setStatistic] = useState([[[], [], [], [], []], [[], [], [], [], []], [[], [], [], [], []]]);
+        const [score, setScore] = useState([])
+        useEffect(() => {
+            const fetchstudentStatistics = async () => {
+                const res = await parentChildStatisticsAxios(child.id);
+                changeParentChaildStatistic(res);
+            };
+            fetchstudentStatistics();
+        }, []);
+    }
 
     return (
         <>
@@ -42,46 +69,23 @@ export default function DetailpageChildProfileCard({ child }) {
                         <h1 className={styles.profileName}>{child.name}</h1>
                     </div>
                 </div>
-
-                <div className={styles.inputTotal}>
-                    <div className={styles.inputContainer}>
-                        <div className={styles.detailBox}>
-                            <DetailBox
-                                onClick={() => handleDetailBoxClick('correctAnswerRate')}
-                                text="분야별, 레벨별 정답률"
-                                width={220}
-                                height={92}
-                            />
+                <div>
+                    <div className={styles.inputTotal}>
+                        <div className={styles.MyPagePink}>
+                            {myPageList.map((data, index) => (
+                                <ClickedPinkBtn
+                                    key={index}
+                                    width={"100"}
+                                    height={"15"}
+                                    text={data}
+                                    idx={index + 1}
+                                    onClick={() =>
+                                        router.push(`parent/child/${child.id}/statistic?view=${index + 1}`)
+                                    }
+                                />
+                            ))}
                         </div>
-                    </div>
-                    <div className={styles.inputContainer}>
-                        <div className={styles.detailBox}>
-                            <DetailBox
-                                onClick={() => handleDetailBoxClick('progress')}
-                                text="학습 진도 현황"
-                                width={220}
-                                height={92}
-                            />
-                        </div>
-                    </div>
-                    <div className={styles.inputContainer}>
-                        <div className={styles.detailBox}>
-                            <DetailBox
-                                onClick={() => handleDetailBoxClick('comparison')}
-                                text="학습 성취도 비교"
-                                width={220}
-                                height={92}
-                            />
-                        </div>
-                    </div>
-                    <div className={styles.inputContainer}>
-                        <div className={styles.detailBox}>
-                            <DetailBox
-                                onClick={() => handleDetailBoxClick('snapshot')}
-                                text="스냅샷"
-                                width={220}
-                                height={92}
-                            />
+                        <div className={styles.MyPageYellow}>
                         </div>
                     </div>
                 </div>
