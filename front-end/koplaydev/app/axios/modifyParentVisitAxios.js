47d30bce3854c7cfe25dc0989e206422fdfea642
jeonsm22 <jeonsm22@daum.net>
2024-08-08 09:05:40 +0900

    fix : first visit first child
    첫 방문에 자식 한 명이 생겨있는 버그 수정

---
+++ front-end/koplaydev/app/axios/modifyParentVisitAxios.js
@@ -0,0 +1,14 @@
+import API from "../utils/API";
+
+export default async function modifyParentVisitAxios() {
+  return API.put("/parent/visit")
+    .then((res) => {
+        console.log(res.data)
+      return res.data.data;
+    })
+    .catch((e) => {
+      console.log(e);
+      console.log("error Handling");
+      return null;
+    });
+}
