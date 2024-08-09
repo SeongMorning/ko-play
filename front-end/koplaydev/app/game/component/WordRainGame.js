d1c6f9ac6392583f4cdacfb1558dbd910fd7930d
jeonsm22 <jeonsm22@daum.net>
2024-08-09 21:30:38 +0900

    fix : wordrain game
    wordrain game이 중간 단계가 다른 컴포넌트 들어가 있어서 수정합니다.

--- front-end/koplaydev/app/game/component/WordRainGame.js
+++ front-end/koplaydev/app/game/component/WordRainGame.js
@@ -4,14 +4,13 @@ import { useSelector } from "react-redux"
 import GameStartBtn from "./GameStartBtn";
 import WordRainStart from "./WordRainStart";
 import WordRainEnd from "./WordRainEnd";
-import RankTest2 from "./rankgame/RankTest2";
 
 export default function WordRainGame(){
     const loading = useSelector((state)=> state.loading);
     if(loading === -1){
         return <GameStartBtn/>
     }else if(loading === 0){
-        return <RankTest2/>
+        return <WordRainStart/>
     }else if(loading === 1){
         return <WordRainEnd />
     }else{
