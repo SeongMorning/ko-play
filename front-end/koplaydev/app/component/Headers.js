ab0ff38198c17a69fb4f9ab26262dc3ea5ba6f05
jiyoung-Heo <hgyoung06@gmail.com>
2024-08-09 15:41:17 +0900

    Reapply "Merge branch '석재륜' into 'master'"
    This reverts commit 2c87c03f7af1b2533994751abc96b4254b3dc080.

--- front-end/koplaydev/app/component/Headers.js
+++ front-end/koplaydev/app/component/Headers.js
@@ -2,39 +2,47 @@
 
 import { usePathname } from "next/navigation";
 import styles from "./Header.module.scss";
-import {motion} from "framer-motion";
+import { motion } from "framer-motion";
 
 export default function Headers() {
   const pathName = usePathname();
+  const exceptList = ["game", "tutorial"];
+
+  const shouldHideHeader = exceptList.some((word) => pathName.includes(word));
+  // slice를 setNation 으로 바꾸면 안되겠지...
+  // default
   return (
     <>
-      {pathName.split("/")[1] === "game" ? null : (
+      {!shouldHideHeader && (
         <div className={styles.header}>
-          <motion.img 
-          className={styles.korea} 
-          src="/korea-3.png"
-          whileHover={{
-            scale : 1.1
-          }} />
-          <motion.img 
-          className={styles.usa} 
-          src="/thailand-parent-choice.png"
-          whileHover={{
-            scale : 1.1
-          }} />
-          <motion.img 
-          className={styles.china} 
-          src="/china-3.png"
-          whileHover={{
-            scale : 1.1
-          }} />
-          <motion.img 
-          className={styles.vietnam} 
-          src="/vietnam-3.png"
-          whileHover={{
-            scale : 1.1
-          }} />
-
+          <motion.img
+            className={styles.korea}
+            src="/korea-3.png"
+            whileHover={{
+              scale: 1.1,
+            }}
+          />
+          <motion.img
+            className={styles.usa}
+            src="/thailand-parent-choice.png"
+            whileHover={{
+              scale: 1.1,
+            }}
+          />
+          <motion.img
+            className={styles.china}
+            src="/china-3.png"
+            whileHover={{
+              scale: 1.1,
+            }}
+          />
+          <motion.img
+            className={styles.vietnam}
+            src="/vietnam-3.png"
+            whileHover={{
+              scale: 1.1,
+            }}
+          />
         </div>
       )}
     </>
