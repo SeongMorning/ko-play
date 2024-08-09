27b5289c78694afded5b110e3a139e2f7c9244c2
jiyoung-Heo <hgyoung06@gmail.com>
2024-08-09 22:35:36 +0900

    test: multi game

--- back-end/koplaydev/src/main/java/com/edu/koplay/websocket/config/WebSocketConfig.java
+++ back-end/koplaydev/src/main/java/com/edu/koplay/websocket/config/WebSocketConfig.java
@@ -2,6 +2,7 @@ package com.edu.koplay.websocket.config;
 
 
 
+import com.edu.koplay.config.ServerConfig;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
@@ -12,7 +13,11 @@ import org.springframework.web.socket.server.support.HttpSessionHandshakeInterce
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+    private final ServerConfig config;
 
+    public WebSocketConfig(ServerConfig config) {
+        this.config = config;
+    }
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
         config.enableSimpleBroker("/topic");
@@ -24,7 +29,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 //        registry.addEndpoint("/gs-guide-websocket").withSockJS();
 //        registry.addEndpoint("/gs-guide-websocket").setAllowedOriginPatterns("*").withSockJS();
 //        registry.addEndpoint("/gs-guide-websocket").setAllowedOriginPatterns("*");
-        registry.addEndpoint("/gs-guide-websocket").setAllowedOrigins("http://192.168.31.173:3000","http://192.168.31.173:8080","http://127.0.0.1:8080","http://localhost:8080","http://127.0.0.1:5500", "http://localhost:5500","http://localhost:3000","http://127.0.0.1:3000","http://172.30.1.27:3000", "http://172.30.1.27:8080", "http://192.168.31.189:8080", "http://192.168.31.189:3000") // 허용할 출처 추가
+        registry.addEndpoint("/gs-guide-websocket").setAllowedOrigins(config.getBack(),"http://192.168.31.173:3000","http://192.168.31.173:8080","http://127.0.0.1:8080","http://localhost:8080","http://127.0.0.1:5500", "http://localhost:5500","http://localhost:3000","http://127.0.0.1:3000","http://172.30.1.27:3000", "http://172.30.1.27:8080", "http://192.168.31.189:8080", "http://192.168.31.189:3000") // 허용할 출처 추가
                 .addInterceptors(new HttpSessionHandshakeInterceptor())
                 .withSockJS();
     }
