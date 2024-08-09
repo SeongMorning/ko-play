d1c6783df1466b0b74a5a010e5f26d9108af5805
jiyoung-Heo <hgyoung06@gmail.com>
2024-08-09 09:02:53 +0900

    fix: change frame variable

--- front-end/koplaydev/app/fame/component/Podium.js
+++ front-end/koplaydev/app/fame/component/Podium.js
@@ -3,6 +3,7 @@ import styles from "./Podium.module.scss";
 
 // props : left, top, users[name, image, plays, level]
 export default function Podium(props) {
+
   return (
     <>
       <div
@@ -11,33 +12,39 @@ export default function Podium(props) {
       >
         <img src="/podium.png" alt="시상대" />
         <div className={`${styles.face1}`}>
-          <img src={props.users.user1.image} alt="1등" />
+          <img src={props.users[0].student.profileImg|| "hehe.png"} alt="1등"
+          onError={(e) => { e.target.src = "hehe.png"; }}
+          />
         </div>
         <div className={styles.light1}></div>
         <div className={`${styles.face2}`}>
-          <img src={props.users.user2.image} alt="2등" />
+          <img src={props.users[1].student.profileImg|| "hehe.png"} alt="2등"
+           onError={(e) => { e.target.src = "hehe.png"; }}
+          />
         </div>
         <div className={styles.light2}></div>
         <div className={`${styles.face3}`}>
-          <img src={props.users.user3.image} alt="3등" />
+          <img src={props.users[2].student.profileImg|| "hehe.png"} alt="3등" 
+          onError={(e) => { e.target.src = "hehe.png"; }}
+ />
         </div>
         <div className={styles.light3}></div>
       </div>
       <div className={styles.info}>
         <div className={`${styles.info} ${styles.info1}`}>
-          이름 : {props.users.user1.name}
+          이름 : {props.users[0].student.name}
           <br />
-          주간 판 수 : {props.users.user1.plays}
+          주간 판 수 : {props.users[0].gameCount}
         </div>
         <div className={`${styles.info} ${styles.info2}`}>
-          이름 : {props.users.user2.name}
+          이름 : {props.users[1].student.name}
           <br />
-          주간 판 수 : {props.users.user2.plays}
+          주간 판 수 : {props.users[1].gameCount}
         </div>
         <div className={`${styles.info} ${styles.info3}`}>
-          이름 : {props.users.user3.name}
+          이름 : {props.users[2].student.name}
           <br />
-          주간 판 수 : {props.users.user3.plays}
+          주간 판 수 : {props.users[2].gameCount}
         </div>
       </div>
     </>
