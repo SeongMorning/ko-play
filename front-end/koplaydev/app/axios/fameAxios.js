f5d1b4be87566669262b636ea8db6a7c1aeb3c2c
jiyoung-Heo <hgyoung06@gmail.com>
2024-08-09 09:02:53 +0900

    fix: change frame variable

--- front-end/koplaydev/app/axios/fameAxios.js
+++ front-end/koplaydev/app/axios/fameAxios.js
@@ -3,7 +3,7 @@ import API from "../utils/API";
 export default async function fameAxios() {
   try {
     const { data } = await API.get("/games/loadTop3students");
-    return data;
+    return data.data;
   } catch (error) {
     // Error Handling
     console.log(error);
