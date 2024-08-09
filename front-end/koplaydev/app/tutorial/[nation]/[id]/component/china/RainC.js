676b2bf13a2b69047e8d1448ba4cad4cd5a1797f
seokjr <116258012+seokjr@users.noreply.github.com>
2024-08-09 17:49:13 +0900

    feat : tutorial return
    튜토리얼 다시 원복

--- front-end/koplaydev/app/tutorial/[nation]/[id]/component/china/RainC.js
+++ front-end/koplaydev/app/tutorial/[nation]/[id]/component/china/RainC.js
@@ -8,12 +8,20 @@ export default function RainC() {
   const router = useRouter();
 
   const tutorialImages = [
-    "/images/tutorial1.png",
-    "/images/tutorial2.png",
-    "/images/tutorial3.png",
-    // 필요한 만큼 이미지를 추가합니다.
+    "https://ko-play.s3.ap-northeast-2.amazonaws.com/tutorial/nation/china/commonC1.png",
+    "https://ko-play.s3.ap-northeast-2.amazonaws.com/tutorial/nation/china/commonC2.png",
+    "https://ko-play.s3.ap-northeast-2.amazonaws.com/tutorial/nation/china/commonC3.png",
+    "https://ko-play.s3.ap-northeast-2.amazonaws.com/tutorial/nation/china/wordrain/rainC1.png",
+    "https://ko-play.s3.ap-northeast-2.amazonaws.com/tutorial/nation/china/wordrain/rainC2.png",
+    "https://ko-play.s3.ap-northeast-2.amazonaws.com/tutorial/nation/china/wordrain/rainC3.png",
+    "https://ko-play.s3.ap-northeast-2.amazonaws.com/tutorial/nation/china/wordrain/rainC4.png",
+    "https://ko-play.s3.ap-northeast-2.amazonaws.com/tutorial/nation/china/commonC4.png",
+    "https://ko-play.s3.ap-northeast-2.amazonaws.com/tutorial/nation/china/commonC5.png",
+    "https://ko-play.s3.ap-northeast-2.amazonaws.com/tutorial/nation/china/commonC6.png",
+    "https://ko-play.s3.ap-northeast-2.amazonaws.com/tutorial/nation/china/commonC7.png",
+    "https://ko-play.s3.ap-northeast-2.amazonaws.com/tutorial/nation/china/commonC8.png",
+    "https://ko-play.s3.ap-northeast-2.amazonaws.com/tutorial/nation/china/commonC9.png",
   ];
-
   const handleNextStep = () => {
     if (currentStep < tutorialImages.length - 1) {
       setCurrentStep(currentStep + 1);
