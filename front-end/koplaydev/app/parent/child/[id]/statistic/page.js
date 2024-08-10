29db9729a5114e9f631ddfed3577752f90737c22
seokjr <116258012+seokjr@users.noreply.github.com>
2024-08-10 17:21:24 +0900

    style : parent page css edit
    부모 페이지 통계관련된 css 싹다 바꿈.

--- front-end/koplaydev/app/parent/child/[id]/statistic/page.js
+++ front-end/koplaydev/app/parent/child/[id]/statistic/page.js
@@ -1,48 +1,49 @@
 "use client";
-import { useSearchParams } from 'next/navigation';
+import { useSearchParams } from "next/navigation";
 import BackScoreBtn from "@/app/component/buttons/BackScoreBtn";
 import styles from "./page.module.scss";
 import StatisticBg from "@/app/component/background/StatisticBg";
-import CorrectAnswerRate from './component/CorrectAnswerRate';
-import Correct from './component/Correct';
-import Progress from './component/Progress';
+import CorrectAnswerRate from "./component/CorrectAnswerRate";
+import Correct from "./component/Correct";
+import Progress from "./component/Progress";
 
 export default function Statistic({ params }) {
-    const id = params.id;
-    const searchParams = useSearchParams();
-    const viewIdx = searchParams.get('view');
+  const id = params.id;
+  const searchParams = useSearchParams();
+  const viewIdx = searchParams.get("view");
 
-    const renderContent = () => {
-        switch (viewIdx) {
-            case '1':
-                return <Correct />;
-            case '2':
-                return <Progress />
-            case '3':
-                return <CorrectAnswerRate />
-            case 'snapshot':
-                return <div>추가예정</div>;
-            default:
-                return <div>선택된 내용이 없습니다.</div>;
-        }
-    };
+  const renderContent = () => {
+    switch (viewIdx) {
+      case "1":
+        return <Correct />;
+      case "2":
+        return <Progress />;
+      case "3":
+        return <CorrectAnswerRate />;
+      case "snapshot":
+        return <div>추가예정</div>;
+      default:
+        return <div>선택된 내용이 없습니다.</div>;
+    }
+  };
 
-    return (
-        <>
-            <div>
-                <BackScoreBtn className={styles.backButton} left={27} top={20} text="뒤로가기" />
-            </div>
-            <div className={styles.data}>
-                <div>
-                    <img className={styles.boardImg} src="/databoard.png" alt="" />
-                    {renderContent()}
-                </div>
-            </div>
-            <div className={styles.characterSection}>
-                <img className={styles.bubble} src="/bubble2.png" alt="" />
-                <img className={styles.character} src="/hehe.png" alt="" />
-            </div>
-            <StatisticBg />
-        </>
-    );
+  return (
+    <>
+      <BackScoreBtn
+        className={styles.backButton}
+        left={27}
+        top={20}
+        text="뒤로가기"
+      />
+      <div className={styles.data}>
+        <img className={styles.boardImg} src="/databoard.png" alt="" />
+        {renderContent()}
+      </div>
+      <div className={styles.characterSection}>
+        <img className={styles.bubble} src="/bubble2.png" alt="" />
+        <img className={styles.character} src="/hehe.png" alt="" />
+      </div>
+      <StatisticBg />
+    </>
+  );
 }
