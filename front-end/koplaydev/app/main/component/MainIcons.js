e0a34fb7accfac08e09ff53112f7692cf4dd4209
seokjr <116258012+seokjr@users.noreply.github.com>
2024-08-10 20:20:34 +0900

    feat : main menu tooltip add
    메인메뉴 전체 툴팁 업데이트

--- front-end/koplaydev/app/main/component/MainIcons.js
+++ front-end/koplaydev/app/main/component/MainIcons.js
@@ -21,30 +21,36 @@ export default function MainIcons() {
           },
         }}
       />
-      <motion.img
-        className={styles.normalGame}
-        src="/realnormal.png"
-        alt=""
-        onClick={() => {
-          dispatch(changeModalIdx(1));
-        }}
-        whileHover={{
-          scale: 1.1,
-          rotate: "10deg",
-        }}
-      />
-      <motion.img 
-      className={styles.rankGame} 
-      src="rank-game.png" 
-      alt=""
-      onClick={()=>{
-        dispatch(changeModalIdx(4));
-      }}
-      whileHover={{
-        scale : 1.1,
-        rotate : "-10deg",
-      }}
-      />
+      <div className={styles.normalGameContainer}>
+        <motion.img
+          className={styles.normalGame}
+          src="/realnormal.png"
+          alt=""
+          onClick={() => {
+            dispatch(changeModalIdx(1));
+          }}
+          whileHover={{
+            scale: 1.1,
+            rotate: "10deg",
+          }}
+        />
+        <div className={styles.normalGameText}>일반게임</div>
+      </div>
+      <div className={styles.rankGameContainer}>
+        <motion.img
+          className={styles.rankGame}
+          src="rank-game.png"
+          alt=""
+          onClick={() => {
+            dispatch(changeModalIdx(4));
+          }}
+          whileHover={{
+            scale: 1.1,
+            rotate: "-10deg",
+          }}
+        />
+        <div className={styles.rankGameText}>랭크게임</div>
+      </div>
       <motion.img
         className={styles.speechBubble}
         src="speechBubble.png"
