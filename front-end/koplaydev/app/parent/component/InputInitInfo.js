9577ac89adbab4c6fb6039d6273b371e1c8ed480
Jiwan Seo <91694379+wannaseoji@users.noreply.github.com>
2024-08-11 00:28:35 +0900

    fix : 쫌쫌따리 수정
    fix : 쫌쫌따리 수정

--- front-end/koplaydev/app/parent/component/InputInitInfo.js
+++ front-end/koplaydev/app/parent/component/InputInitInfo.js
@@ -87,7 +87,7 @@ export default function InputInitInfo({ onclose, setChildInfo }) {
     return (
         <>
             <div className={styles.overlay} ></div>
-            <div className={styles.modalBg}></div>
+            
             <div className={styles.modal}>
                 <div className={styles.modalContent}>
                     <h2>초기 정보 입력</h2>
@@ -103,11 +103,14 @@ export default function InputInitInfo({ onclose, setChildInfo }) {
                                     <tr><th colSpan="3">듣기(Listening)</th></tr>
                                 </thead>
                                 <tbody>
-                                    <tr><td className={styles.tableNumber}>1.</td><td className={styles.tableContent}>자녀가 일상 대화를 이해할 수 있나요?</td><td className={styles.checkboxColumn}><label  className={styles.customRadio}><input type="radio" name='listening1' value='yes'onChange={handleRadioChange} />예</label> <label><input type="radio" name='listening1' value='no' onChange={handleRadioChange}/>아니오</label></td></tr>
-                                    <tr><td className={styles.tableNumber}>2.</td><td className={styles.tableContent}>자녀가 간단한 명령이나 지시를 이해할 수 있나요?</td><td className={styles.checkboxColumn}><label><input type="radio" name='listening2' value='yes'onChange={handleRadioChange} />예</label> <label><input type="radio"  name='listening2' value='no' onChange={handleRadioChange}/>아니오</label></td></tr>
-                                    <tr><td className={styles.tableNumber}>3.</td><td className={styles.tableContent}>자녀가 짧은 이야기나 설명을 이해할 수 있나요?</td><td className={styles.checkboxColumn}><label><input type="radio" name='listening3' value='yes' onChange={handleRadioChange}/>예</label> <label><input type="radio"  name='listening3' value='no'onChange={handleRadioChange} />아니오</label></td></tr>
-                                    <tr><td className={styles.tableNumber}>4.</td><td className={styles.tableContent}>자녀가 국어(또는 학습할 언어)로 된 노래나 동화를 듣고 이해할 수 있는 정도는 어느 정도인가요?</td>
-                                        <td className={styles.checkboxColumn}>
+                                    <tr><td className={styles.tableContent}>1. 자녀가 일상 대화를 이해할 수 있나요?</td></tr>
+                                    <tr><td className={styles.checkboxColumn}><label  className={styles.customRadio}><input type="radio" name='listening1' value='yes'onChange={handleRadioChange} />예</label> <label><input type="radio" name='listening1' value='no' onChange={handleRadioChange}/>아니오</label></td>   </tr>
+                                    <tr><td className={styles.tableContent}>2. 자녀가 간단한 명령이나 지시를 이해할 수 있나요?</td></tr>
+                                    <tr><td className={styles.checkboxColumn}><label className={styles.customRadio}><input type="radio" name='listening2' value='yes'onChange={handleRadioChange} />예</label> <label><input type="radio"  name='listening2' value='no' onChange={handleRadioChange}/>아니오</label></td></tr>
+                                    <tr><td className={styles.tableContent}>3. 자녀가 짧은 이야기나 설명을 이해할 수 있나요?</td></tr>
+                                    <tr><td className={styles.checkboxColumn}><label className={styles.customRadio}><input type="radio" name='listening3' value='yes' onChange={handleRadioChange}/>예</label> <label><input type="radio"  name='listening3' value='no'onChange={handleRadioChange} />아니오</label></td></tr>
+                                    <tr><td className={styles.tableContent}>4. 자녀가 국어(또는 학습할 언어)로 된 노래나 동화를 듣고 이해할 수 있는 정도는 어느 정도인가요?</td></tr>
+                                    <tr> <td className={styles.checkboxColumn}>
                                             <div className={styles.threeCheck}><label> <input type="radio"  name='listening4' value='yes'  onChange={handleRadioChange}/>잘 이해한다</label><label><input type="radio"  name='listening4' value='so' onChange={handleRadioChange}/>보통이다</label><label><input type="radio" name='listening4' value='no'  onChange={handleRadioChange}/>거의 이해하지 못한다</label>
                                             </div></td></tr>
                                 </tbody>
@@ -119,12 +122,15 @@ export default function InputInitInfo({ onclose, setChildInfo }) {
                                     <tr><th colSpan="3">읽기(Reading)</th></tr>
                                 </thead>
                                 <tbody>
-                                    <tr><td className={styles.tableNumber}>1.</td><td className={styles.tableContent}>자녀가 간단한 문장을 읽고 이해할 수 있나요?</td><td className={styles.checkboxColumn}><label><input type="radio" name='reading1' value='yes'onChange={handleRadioChange} />예</label> <label><input type="radio" name='reading1' value='no' onChange={handleRadioChange}/>아니오</label></td></tr>
-                                    <tr><td className={styles.tableNumber}>2.</td><td className={styles.tableContent}>자녀가 짧은 이야기나 동화를 읽을 수 있나요?</td><td className={styles.checkboxColumn}><label><input type="radio" name='reading2' value='yes' onChange={handleRadioChange}/>예</label> <label><input type="radio" name='reading2' value='no' onChange={handleRadioChange}/>아니오</label></td></tr>
-                                    <tr><td className={styles.tableNumber}>3.</td><td className={styles.tableContent}>자녀가 길고 복잡한 문장을 읽을 수 있나요?</td><td className={styles.checkboxColumn}><label><input type="radio" name='reading3' value='yes' onChange={handleRadioChange}/>예</label> <label><input type="radio" name='reading3' value='no'onChange={handleRadioChange} />아니오</label></td></tr>
-                                    <tr><td className={styles.tableNumber}>4.</td><td className={styles.tableContent}>자녀가 국어(또는 학습할 언어)로 된 간단한 책을 읽을 수 있는 정도는 어느 정도인가요?</td>
-                                        <td className={styles.checkboxColumn}>
-                                            <div className={styles.threeCheck}><label>  <input  type="radio" name='reading4' value='yes' onChange={handleRadioChange}/>잘 읽는다</label><label><input type="radio" name='reading4' value='so'onChange={handleRadioChange} />보통이다</label><label><input  type="radio" name='reading4' value='no'onChange={handleRadioChange} />거의 읽지 못한다</label>
+                                    <tr><td className={styles.tableContent}>1. 자녀가 간단한 문장을 읽고 이해할 수 있나요?</td></tr>
+                                    <tr><td className={styles.checkboxColumn}><label  className={styles.customRadio}><input type="radio" name='listening1' value='yes'onChange={handleRadioChange} />예</label> <label><input type="radio" name='listening1' value='no' onChange={handleRadioChange}/>아니오</label></td>   </tr>
+                                    <tr><td className={styles.tableContent}>2. 자녀가 짧은 이야기나 동화를 읽을 수 있나요?</td></tr>
+                                    <tr><td className={styles.checkboxColumn}><label className={styles.customRadio}><input type="radio" name='listening2' value='yes'onChange={handleRadioChange} />예</label> <label><input type="radio"  name='listening2' value='no' onChange={handleRadioChange}/>아니오</label></td></tr>
+                                    <tr><td className={styles.tableContent}>3. 자녀가 길고 복잡한 문장을 읽을 수 있나요?</td></tr>
+                                    <tr><td className={styles.checkboxColumn}><label className={styles.customRadio}><input type="radio" name='listening3' value='yes' onChange={handleRadioChange}/>예</label> <label><input type="radio"  name='listening3' value='no'onChange={handleRadioChange} />아니오</label></td></tr>
+                                    <tr><td className={styles.tableContent}>4. 자녀가 국어(또는 학습할 언어로)로 된 간단한 책을 읽을 수 있는 정도는 어느정도인가요?</td></tr>
+                                    <tr> <td className={styles.checkboxColumn}>
+                                            <div className={styles.threeCheck}><label> <input type="radio"  name='listening4' value='yes'  onChange={handleRadioChange}/>잘 이해한다</label><label><input type="radio"  name='listening4' value='so' onChange={handleRadioChange}/>보통이다</label><label><input type="radio" name='listening4' value='no'  onChange={handleRadioChange}/>거의 이해하지 못한다</label>
                                             </div></td></tr>
                                 </tbody>
                             </table>
@@ -134,13 +140,16 @@ export default function InputInitInfo({ onclose, setChildInfo }) {
                                     <tr><th colSpan="3">말하기(Speaking)</th></tr>
                                 </thead>
                                 <tbody>
-                                    <tr><td className={styles.tableNumber}>1.</td><td className={styles.tableContent3}>자녀가 간단한 인사말이나 자기소개를 영어(또는 학습할 언어)로 할 수 있나요?</td><td className={styles.checkboxColumn}><label><input type="radio" name='speaking1' value='yes' onChange={handleRadioChange}/>예</label> <label><input type="radio" name='speaking1' value='no' onChange={handleRadioChange}/>아니오</label></td></tr>
-                                    <tr><td className={styles.tableNumber}>2.</td><td className={styles.tableContent3}>자녀가 일상적인 대화를 영어(또는 학습할 언어)로 할 수 있나요?</td><td className={styles.checkboxColumn}><label><input type="radio" name='speaking2' value='yes' onChange={handleRadioChange}/>예</label> <label><input type="radio" name='speaking2' value='no'onChange={handleRadioChange} />아니오</label></td></tr>
-                                    <tr><td className={styles.tableNumber}>3.</td><td className={styles.tableContent3}>자녀가 자신의 생각이나 의견을 국어(또는 학습할 언어)로 표현할 수 있나요?</td><td className={styles.checkboxColumn}><label><input type="radio" name='speaking3' value='yes' onChange={handleRadioChange}/>예</label> <label><input type="radio" name='speaking3' value='no' onChange={handleRadioChange}/>아니오</label></td></tr>
-                                    <tr><td className={styles.tableNumber}>4.</td><td className={styles.tableContent4}>자녀가 국어(또는 학습할 언어)로 자신의 일상생활이나 경험을 이야기할 수 있는 정도는 어느 정도인가요?</td>
-                                        <td className={styles.checkboxColumn}>
-                                            <div className={styles.threeCheck}><label> <input type="radio" name='speaking4' value='yes' onChange={handleRadioChange}/>잘 이야기한다</label><label><input type="radio" name='speaking4' value='so' onChange={handleRadioChange}/>보통이다</label><label><input type="radio" name='speaking4' value='no' onChange={handleRadioChange}/>거의 이야기하지 못한다</label></div>
-                                        </td></tr>
+                                    <tr><td className={styles.tableContent}>1. 자녀가 간단한 인사말이나 자기소개를 한국어(또는 학습할 언어)로 할 수 있나요?</td></tr>
+                                    <tr><td className={styles.checkboxColumn}><label  className={styles.customRadio}><input type="radio" name='listening1' value='yes'onChange={handleRadioChange} />예</label> <label><input type="radio" name='listening1' value='no' onChange={handleRadioChange}/>아니오</label></td>   </tr>
+                                    <tr><td className={styles.tableContent}>2. 자녀가 일상적인 대화를 국어(또는 학습할 언어)로 할 수 있나요?</td></tr>
+                                    <tr><td className={styles.checkboxColumn}><label className={styles.customRadio}><input type="radio" name='listening2' value='yes'onChange={handleRadioChange} />예</label> <label><input type="radio"  name='listening2' value='no' onChange={handleRadioChange}/>아니오</label></td></tr>
+                                    <tr><td className={styles.tableContent}>3. 자녀가 자신의 생각이나 의견을 국어(또는 학습할 언어로)표현할 수 있나요?</td></tr>
+                                    <tr><td className={styles.checkboxColumn}><label className={styles.customRadio}><input type="radio" name='listening3' value='yes' onChange={handleRadioChange}/>예</label> <label><input type="radio"  name='listening3' value='no'onChange={handleRadioChange} />아니오</label></td></tr>
+                                    <tr><td className={styles.tableContent}>4. 자녀가 국어(또는 학습할 언어)로 자신의 일상생활이나 경험을 이야기할 수 있는 정도는 어느정도인가요?</td></tr>
+                                    <tr> <td className={styles.checkboxColumn}>
+                                            <div className={styles.threeCheck}><label> <input type="radio"  name='listening4' value='yes'  onChange={handleRadioChange}/>잘 이해한다</label><label><input type="radio"  name='listening4' value='so' onChange={handleRadioChange}/>보통이다</label><label><input type="radio" name='listening4' value='no'  onChange={handleRadioChange}/>거의 이해하지 못한다</label>
+                                            </div></td></tr>
                                 </tbody>
                             </table>
                         </div>
