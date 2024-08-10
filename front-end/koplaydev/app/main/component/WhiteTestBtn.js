cfab2f2afd0b97899939d83da6072c028de2ee42
kjiin <156158673+kjiin@users.noreply.github.com>
2024-08-10 22:02:23 +0900

    feat : Add audios
    - public/audios에 효과음 추가
    - effectSound 유틸 함수 추가
    - useSound 훅 추가

--- front-end/koplaydev/app/main/component/WhiteTestBtn.js
+++ front-end/koplaydev/app/main/component/WhiteTestBtn.js
@@ -1,14 +1,21 @@
 import { changeTestIdx } from "@/redux/slices/testSlice";
 import styles from "./WhiteTestBtn.module.scss";
 import { useDispatch, useSelector } from "react-redux";
+import effectSound from '@/app/utils/effectSound'
+
+const settingTestButtonSound = '/audios/settingTestButtonSound.mp3';
 
 export default function WhiteTestBtn(props) {
   const testIdx = useSelector((state) => state.test);
   const dispatch = useDispatch();
+
+  const es = effectSound(settingTestButtonSound, 1);
+
   return (
     <>
       <div
         onClick={() => {
+          es.play();
           dispatch(changeTestIdx(props.idx + 1));
         }}
         className={`${styles.WhiteTestBtn} ${props.idx + 1 === testIdx ? styles.click : ""}`}
