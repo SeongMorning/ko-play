6dce3e4fecb29d6347182487c67f07309458847a
jeonsm22 <jeonsm22@daum.net>
2024-08-11 23:34:36 +0900

    feat : rankgame (100%)
    랭크게임 진행, 촬영 까지 다 했습니다.

--- front-end/koplaydev/app/game/component/RankGameStartBtn.js
+++ front-end/koplaydev/app/game/component/RankGameStartBtn.js
@@ -1,9 +1,12 @@
 "use client";
+import "regenerator-runtime/runtime";
+import SpeechRecognition, { useSpeechRecognition } from "react-speech-recognition";
 import { useState, useEffect } from "react";
 import styles from "./RankGameStartBtn.module.scss";
 import { useDispatch, useSelector } from "react-redux";
 import { changeLoadingIdx } from "@/redux/slices/loadingSlice";
 import { changeGameWord } from "@/redux/slices/gameWordSlice";
+import OpenViduItem from "@/app/utils/openvidu/OpenVidu";
 
 export default function RankGameStartBtn() {
   const [countdown, setCountdown] = useState(null);
@@ -12,16 +15,17 @@ export default function RankGameStartBtn() {
   const wordList = useSelector((state) => state.gameWord);
   const wordleft = useSelector((state) => state.gameLeft);
 
-  useEffect(()=>{
-    const copy = wordList.map((word) => ({...word}));
+  useEffect(() => {
+    const copy = wordList.map((word) => ({ ...word }));
     copy.map((word, index) => {
       word["left"] = wordleft[index].left;
       word["state"] = wordleft[index].state;
-    })
+    });
     console.log(copy);
     dispatch(changeGameWord(copy));
     handleStartClick();
-  },[])
+    SpeechRecognition.startListening({ language: "ko-KR", continuous: true });
+  }, []);
 
   useEffect(() => {
     let timer;
@@ -37,31 +41,33 @@ export default function RankGameStartBtn() {
   }, [countdown]);
 
   const handleStartClick = () => {
-    setCountdown(3);
+    setCountdown(5);
   };
 
   return (
-    <div className={styles.GameStartBtn} onClick={handleStartClick}>
-      {!gameStarted ? (
-        <>
-          {countdown === null ? (
-            <>
-              <div className={styles.GameStartBtnTop}>시작</div>
-              <div className={styles.GameStartBtnBottom}>시작</div>
-            </>
-          ) : (
-            <>
-              <div className={styles.GameStartBtnTop}>{countdown}</div>
-              <div className={styles.GameStartBtnBottom}>{countdown}</div>
-            </>
-          )}
-        </>
-      ) : (
-        <>
-          <div className={styles.GameStartBtnTop}>게임 화면!</div>
-          <div className={styles.GameStartBtnBottom}>게임 화면!</div>
-        </>
-      )}
-    </div>
+    <>
+      <div className={styles.GameStartBtn} onClick={handleStartClick}>
+        {!gameStarted ? (
+          <>
+            {countdown === null ? (
+              <>
+                <div className={styles.GameStartBtnTop}>시작</div>
+                <div className={styles.GameStartBtnBottom}>시작</div>
+              </>
+            ) : (
+              <>
+                <div className={styles.GameStartBtnTop}>{countdown}</div>
+                <div className={styles.GameStartBtnBottom}>{countdown}</div>
+              </>
+            )}
+          </>
+        ) : (
+          <>
+            <div className={styles.GameStartBtnTop}>게임 화면!</div>
+            <div className={styles.GameStartBtnBottom}>게임 화면!</div>
+          </>
+        )}
+      </div>
+    </>
   );
 }
