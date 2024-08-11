659cd304f069a215f8295fca8008818663a5f6f1
jiyoung-Heo <hgyoung06@gmail.com>
2024-08-11 20:56:14 +0900

    feat: rollback setting

--- front-end/koplaydev/app/layout.js
+++ front-end/koplaydev/app/layout.js
@@ -2,7 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.scss";
 import Headers from "./component/Headers";
 import ReduxProvider from "@/redux/provider";
-import { useEffect } from 'react';
+// import { useEffect } from 'react';
 
 
 const inter = Inter({ subsets: ["latin"] });
@@ -41,27 +41,28 @@ export const viewport = {
 
 export default function RootLayout({ children }) {
 
-  useEffect(() => {
-    const currentOrientation = () => {
-      if (screen.availHeight > screen.availWidth) {
-        document.documentElement.classList.add('portrait');
-        document.documentElement.classList.remove('landscape');
-      } else {
-        document.documentElement.classList.add('landscape');
-        document.documentElement.classList.remove('portrait');
-      }
-    };
-    // 초기화 시 실행
-    currentOrientation();
+  // useEffect(() => {
+  //   console.log*('test')
+  //   const currentOrientation = () => {
+  //     if (screen.availHeight > screen.availWidth) {
+  //       document.documentElement.classList.add('portrait');
+  //       document.documentElement.classList.remove('landscape');
+  //     } else {
+  //       document.documentElement.classList.add('landscape');
+  //       document.documentElement.classList.remove('portrait');
+  //     }
+  //   };
+  //   // 초기화 시 실행
+  //   currentOrientation();
     
-    // 윈도우 크기 변경 시마다 실행
-    window.addEventListener('resize', currentOrientation);
+  //   // 윈도우 크기 변경 시마다 실행
+  //   window.addEventListener('resize', currentOrientation);
 
-    // 컴포넌트 언마운트 시 이벤트 리스너 제거
-    return () => {
-      window.removeEventListener('resize', currentOrientation);
-    };
-  }, []);
+  //   // 컴포넌트 언마운트 시 이벤트 리스너 제거
+  //   return () => {
+  //     window.removeEventListener('resize', currentOrientation);
+  //   };
+  // }, []);
 
 
   return (
