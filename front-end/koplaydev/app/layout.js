29ebe98d211865f57404efb20f52313ffdd848ba
jiyoung-Heo <hgyoung06@gmail.com>
2024-08-11 21:28:13 +0900

    remove: remove test

--- front-end/koplaydev/app/layout.js
+++ front-end/koplaydev/app/layout.js
@@ -2,34 +2,10 @@ import { Inter } from "next/font/google";
 import "./globals.scss";
 import Headers from "./component/Headers";
 import ReduxProvider from "@/redux/provider";
-// import { useEffect } from 'react';
 
 
 const inter = Inter({ subsets: ["latin"] });
-import { useEffect } from 'react';
 
-useEffect(() => {
-  // 현재 화면 방향을 콘솔에 출력
-  console.log(screen.orientation.type); // logs the current orientation
-
-  // 화면을 가로모드로 잠금
-  screen.orientation.lock('landscape-primary').catch((err) => {
-    console.error('Failed to lock orientation:', err);
-  });
-
-  // 화면 방향 변경 감지
-  const handleOrientationChange = () => {
-    console.log('Orientation Changed');
-  };
-
-  screen.orientation.addEventListener('change', handleOrientationChange);
-
-  // 컴포넌트 언마운트 시 이벤트 리스너 정리 및 화면 잠금 해제
-  return () => {
-    screen.orientation.unlock();
-    screen.orientation.removeEventListener('change', handleOrientationChange);
-  };
-}, []);
 
 export const metadata = {
   title: "Ko-play",
@@ -63,30 +39,6 @@ export const viewport = {
 }
 
 export default function RootLayout({ children }) {
-
-  // useEffect(() => {
-  //   console.log*('test')
-  //   const currentOrientation = () => {
-  //     if (screen.availHeight > screen.availWidth) {
-  //       document.documentElement.classList.add('portrait');
-  //       document.documentElement.classList.remove('landscape');
-  //     } else {
-  //       document.documentElement.classList.add('landscape');
-  //       document.documentElement.classList.remove('portrait');
-  //     }
-  //   };
-  //   // 초기화 시 실행
-  //   currentOrientation();
-    
-  //   // 윈도우 크기 변경 시마다 실행
-  //   window.addEventListener('resize', currentOrientation);
-
-  //   // 컴포넌트 언마운트 시 이벤트 리스너 제거
-  //   return () => {
-  //     window.removeEventListener('resize', currentOrientation);
-  //   };
-  // }, []);
-
 
   return (
     <html lang="ko">
