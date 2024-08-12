7ae2618a67c935bc68dc394f5b08c7fe46ff3de7
jeonsm22 <jeonsm22@daum.net>
2024-08-13 03:32:28 +0900

    feat : add openvidu AR
    마스크가 나오긴 하는데 위치랑 인식이 아직 정확하지 않아서 추가 작업 필요합니다.

--- front-end/koplaydev/app/avatar/component/Cam.js
+++ front-end/koplaydev/app/avatar/component/Cam.js
@@ -1,15 +1,28 @@
 "use client";
 
-import { forwardRef } from "react";
+import { forwardRef, useEffect, useRef } from "react";
 import styles from "./Cam.module.scss";
 import Webcam from "react-webcam"; // react-webcam import
 import { useSelector } from "react-redux";
+import FaceDetection, { initWebcamAndModel } from "@/app/utils/AR/script";
 
 // props : left, top, width
 const Cam = forwardRef((props, ref) => {
+  const webcamRef = useRef(null);
+  const handleInit = (videoElement) => {
+    initWebcamAndModel(videoElement);
+  };
+
+  useEffect(() => {
+    if (webcamRef.current) {
+      handleInit(webcamRef.current.video);
+    }
+  }, [webcamRef]);
+
   return (
     <div ref={ref}>
       <Webcam
+        ref={webcamRef}
         className={styles.webcam}
         style={{
           left: props.left,
@@ -18,8 +31,12 @@ const Cam = forwardRef((props, ref) => {
           height: props.height,
           right: props.right,
           bottom: props.bottom,
+          zIndex: 1,
         }}
       />
+      {webcamRef.current && webcamRef.current.video && (
+        <FaceDetection videoElement={webcamRef.current.video} />
+      )}
     </div>
   );
 });
