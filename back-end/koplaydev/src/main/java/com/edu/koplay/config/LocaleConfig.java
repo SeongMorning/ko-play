b14146200c6269e18a37750cb3e5377870a88977
jiyoung-Heo <hgyoung06@gmail.com>
2024-08-12 21:24:34 +0900

    fix: Change translation store for back
    국제화 단어를 한번에 받아오기 위한 소스 수정합니다.

--- back-end/koplaydev/src/main/java/com/edu/koplay/config/LocaleConfig.java
+++ back-end/koplaydev/src/main/java/com/edu/koplay/config/LocaleConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
@@ -15,11 +16,18 @@ public class LocaleConfig implements WebMvcConfigurer {
 
     @Bean
     public LocaleResolver localeResolver() {
-        // 세션을 통해 로케일을 유지하고, 기본 로케일을 한국어로 설정
-        SessionLocaleResolver slr = new SessionLocaleResolver();
-        slr.setDefaultLocale(new Locale("ko", "KR")); // 기본 로케일 설정
+        // Accept-Language 헤더를 통해 로케일을 설정하는 Resolver
+        AcceptHeaderLocaleResolver slr = new AcceptHeaderLocaleResolver();
+        slr.setDefaultLocale(Locale.KOREA); // 기본 로케일을 한국어로 설정
         return slr;
     }
+//    @Bean
+//    public LocaleResolver localeResolver() {
+//        // 세션을 통해 로케일을 유지하고, 기본 로케일을 한국어로 설정
+//        SessionLocaleResolver slr = new SessionLocaleResolver();
+//        slr.setDefaultLocale(new Locale("ko", "KR")); // 기본 로케일 설정
+//        return slr;
+//    }
 
     @Bean
     public LocaleChangeInterceptor localeChangeInterceptor() {
