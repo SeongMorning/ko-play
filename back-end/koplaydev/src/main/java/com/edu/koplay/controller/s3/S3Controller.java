67715ace41c263049339098cff588a473c01b1ff
jiyoung-Heo <hgyoung06@gmail.com>
2024-08-13 23:00:56 +0900

    feat: Change profile save local to s3

--- back-end/koplaydev/src/main/java/com/edu/koplay/controller/s3/S3Controller.java
+++ back-end/koplaydev/src/main/java/com/edu/koplay/controller/s3/S3Controller.java
@@ -17,10 +17,10 @@ public class S3Controller {
     }
 
     @PostMapping(value = "/upload")
-    public ResponseEntity<?> s3Upload(@RequestPart(value = "image", required = false) MultipartFile image){
+        public ResponseEntity<?> s3Upload(@RequestPart(value = "image", required = false) MultipartFile image, String folder){
 
         try{
-            ResponseEntity<?> profileImage = s3ImageService.upload(image);
+            ResponseEntity<?> profileImage = s3ImageService.upload(image, folder);
             return ResponseEntity.ok(profileImage);
         }catch (Exception e){
             e.printStackTrace();
