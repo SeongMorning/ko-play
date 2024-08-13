79fe08186e04361e53ae9bb37646175b157a14fe
jiyoung-Heo <hgyoung06@gmail.com>
2024-08-13 23:00:24 +0900

    feat: Change profile save local to s3

--- front-end/koplaydev/app/axios/studentInfo.js
+++ front-end/koplaydev/app/axios/studentInfo.js
@@ -2,7 +2,6 @@ import API from "../utils/API";
 
 export default async function student() {
   try {
-    console.log("34");
     const { data } = await API.get("/students/info");
     return data.data[0];
   } catch (error) {
