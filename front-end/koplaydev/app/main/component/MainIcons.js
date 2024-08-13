12feba027d687d198f641125373bdfc05eb84fbe
seokjr <116258012+seokjr@users.noreply.github.com>
2024-08-13 10:26:42 +0900

    fix : mainMenuIcon selectPlanet edit
    메인메뉴 셀렉트플래닛 text화 시킴.

--- front-end/koplaydev/app/main/component/MainIcons.js
+++ front-end/koplaydev/app/main/component/MainIcons.js
@@ -4,12 +4,14 @@ import { useDispatch, useSelector } from "react-redux";
 import styles from "./MainIcons.module.scss";
 import { motion } from "framer-motion";
 import { changeModalIdx } from "@/redux/slices/modalSlice";
-import effectSound from '@/app/utils/effectSound'
+import effectSound from "@/app/utils/effectSound";
 
-const planetButtonSound = 'https://ko-play.s3.ap-northeast-2.amazonaws.com/audio/effect/planetButtonSound.wav';
-const planetButtonSound2 = 'https://ko-play.s3.ap-northeast-2.amazonaws.com/audio/effect/planetButtonSound2.mp3';
-const planetButtonSound3 = 'https://ko-play.s3.ap-northeast-2.amazonaws.com/audio/effect/planetButtonSound3.wav';
-
+const planetButtonSound =
+  "https://ko-play.s3.ap-northeast-2.amazonaws.com/audio/effect/planetButtonSound.wav";
+const planetButtonSound2 =
+  "https://ko-play.s3.ap-northeast-2.amazonaws.com/audio/effect/planetButtonSound2.mp3";
+const planetButtonSound3 =
+  "https://ko-play.s3.ap-northeast-2.amazonaws.com/audio/effect/planetButtonSound3.wav";
 
 export default function MainIcons() {
   const translationWords = useSelector((state) => state.translationWords);
@@ -45,7 +47,9 @@ export default function MainIcons() {
             rotate: "10deg",
           }}
         />
-        <div className={styles.normalGameText}>{translationWords.normalGame}</div>
+        <div className={styles.normalGameText}>
+          {translationWords.normalGame}
+        </div>
       </div>
       <div className={styles.rankGameContainer}>
         <motion.img
@@ -61,14 +65,10 @@ export default function MainIcons() {
             rotate: "-10deg",
           }}
         />
-        <div className={styles.rankGameText}>
-        {translationWords.rankGame}
-        </div>
+        <div className={styles.rankGameText}>{translationWords.rankGame}</div>
       </div>
-      <motion.img
-        className={styles.speechBubble}
-        src="speechBubble.png"
-        alt=""
+      <motion.div
+        className={styles.speechBubbleContainer}
         animate={{
           translateY: [0, -5, 0],
           transition: {
@@ -76,7 +76,10 @@ export default function MainIcons() {
             duration: 2,
           },
         }}
-      />
+      >
+        <img className={styles.speechBubble} src="speechBubble.png" alt="" />
+        <div className={styles.speechText}>{translationWords.selectPlanet}</div>
+      </motion.div>
     </>
   );
 }
