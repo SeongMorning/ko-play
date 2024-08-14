bdd6116961e3402ea592b029aa45c6f26599b537
jiyoung-Heo <hgyoung06@gmail.com>
2024-08-14 23:42:03 +0900

    feat: Add 아바타 관련 api, axios

--- back-end/koplaydev/src/main/java/com/edu/koplay/dto/AvatarDTO.java
+++ back-end/koplaydev/src/main/java/com/edu/koplay/dto/AvatarDTO.java
@@ -15,11 +15,13 @@ public class AvatarDTO {
     private Long avatarIdx;
     private String countryName;
     private String avatarFile;
+    private boolean currUse;
 
     public AvatarDTO(Avatar avatar) {
         this.avatarFile = avatar.getAvatarFile();
         this.countryName = avatar.getNation().getCountryName();
         this.avatarIdx = avatar.getAvatarIdx();
+
     }
 
     public static Avatar toEntity(final AvatarDTO dto) {
