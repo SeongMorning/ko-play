0199db1976a0905776ed9fc7875ac5bb4d5594c8
kjiin <156158673+kjiin@users.noreply.github.com>
2024-08-15 23:53:24 +0900

    feat : Add mute Icon
    - 음소거 버튼 추가
    - 아바타 페이지 배경음악 추가, 게임 배경음악 볼륨 조절

--- front-end/koplaydev/app/avatar/page.js
+++ front-end/koplaydev/app/avatar/page.js
@@ -4,9 +4,13 @@ import { useSelector } from "react-redux";
 import AlbumFameBg from "../component/background/AlbumFameBg";
 import BackScoreBtn from "../component/buttons/BackScoreBtn";
 import Cabinet from "./component/Cabinet";
+import useSound from "@/app/utils/useSound";
+
+const avatarBGM = 'https://ko-play.s3.ap-northeast-2.amazonaws.com/audio/background/albumBGM.wav';
 
 export default function Avatar() {
   const translationWords = useSelector((state) => state.translationWords);
+  useSound(avatarBGM, 0.8, 0);
 
   return (
     <>
